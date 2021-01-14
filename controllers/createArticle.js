4d871bba34d77b689a025b8bc90536cc583ff509
KamiyaNya <dikriss16@gmail.com>
2021-01-14 21:31:39 +0300

    Контроллер для добавления поста в базу данных

---
+++ controllers/createArticle.js
@@ -0,0 +1,37 @@
+const {
+    validationResult
+} = require('express-validator')
+
+const Article = require('../models/Article')
+
+module.exports.createArticle = async function (req, res) {
+    try {
+        const articleTitle = req.body.articleTitle
+        const articleBody = req.body.articleBody
+        const errors = validationResult(res)
+        if (!errors.isEmpty()) {
+            res.status(422).json({
+                error: errors
+            })
+        }
+        if (!articleTitle) {
+            res.status(400).json({
+                message: "Пустой заголовок"
+            })
+        }
+        if (!articleBody) {
+            res.status(400).json({
+                message: "Пустое тело новости"
+            })
+        }
+        const newArticle = Article({
+            articleTitle: articleTitle,
+            articleBody: articleBody
+        })
+        await newArticle.save()
+        res.status(200).send(`Новость добавлена, Тема новости <strong>'${articleTitle}'</strong>, телo новости '<strong>${articleBody}</strong>', <a href='/create_article'>Вернуться</a>`)
+    } catch (e) {
+        console.log(e)
+    }
+
+}
