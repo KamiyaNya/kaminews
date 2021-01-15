8fbed87b95e8528a88b6822180e56cfd78617604
KamiyaNya <dikriss16@gmail.com>
2021-01-15 23:28:35 +0300

    Добавлен паспорт для сайта

--- middleware/passport.js
+++ middleware/passport.js
@@ -2,12 +2,23 @@ const JWTStrategy = require('passport-jwt').Strategy
 const ExstractJWT = require('passport-jwt').ExtractJwt
 
 const JWTkey = require('../config.json').secretJWTKey
+const User = require('../models/User')
 
 const options = {
     jwtFromRequest: ExstractJWT.fromAuthHeaderAsBearerToken(),
     secretOreKey: JWTkey
 }
 
-module.exports = passport = (req, res) => {
-
+module.exports = passport => {
+    passport.use(new JWTStrategy(options, async (payload, done) => {
+        const user = await User.findById({
+            _id: payload.userId
+        })
+        if (!user) {
+            done(err, false)
+        }
+        if (user) {
+            done(null, user)
+        }
+    }))
 }
