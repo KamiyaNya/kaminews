e3457a73b0f5c1c06e3b03e11a459afd78d7d6da
KamiyaNya <dikriss16@gmail.com>
2021-01-15 22:21:20 +0300

    Изменил получение id поста. На странице /news сделать ограничение символов для текста под заголовком

--- controllers/getArticles.js
+++ controllers/getArticles.js
@@ -2,37 +2,50 @@ const Article = require('../models/Article')
 
 module.exports.getArticles = async (req, res) => {
     try {
-        const articles = await Article.find().lean()
+        const articles = await Article.find({}).lean()
+        // const articlesBody = await Article.findOne({
+        //     articleBody: articleBody
+        // })
+        // let shortArticleBody = ''
+
+        // for (let i = 0; i < articlesBody.length; i++) {
+        //     //Написать цикл по перебору срок из бд
+        //     shortArticleBody += articlesBody
+        // }
+        // console.log(shortArticleBody)
         res.render('articlesPage', {
             title: 'Новости',
             articles,
             id: articles._id
         })
-    } catch (error) {
-        if (error) {
-            res.status(503).json({
-                message: `Ошибка сервера : ${error}`
-            })
+    } catch (e) {
+        if (e) {
+            // res.status(503).json({
+            //     message: `Ошибка сервера : ${error}`
+            // })
+            console.log(e)
         }
     }
 
 }
-module.exports.getArticleById = async (req, res) => {
+module.exports.getArticleById = async (req, res, next) => {
+
     try {
         const postId = req.params.id
-        const postIdTrim = postId.split(':').join('')
         const articles = await Article.findById({
-            _id: postIdTrim
+            _id: postId
         })
+        // res.json(req.params)
+        // console.log(postIdTrim)
         res.render('articlePage', {
-            id: postIdTrim,
-            text: articles.articleBody
+            title: articles.articleTitle,
+            id: postId,
+            articleTitle: articles.articleTitle,
+            articleBody: articles.articleBody
         })
     } catch (error) {
         if (error) {
-            res.status(503).json({
-                message: `Ошибка сервера : ${error}`
-            })
+            console.log(error)
         }
     }
 
