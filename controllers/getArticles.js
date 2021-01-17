8bb3d7ae5dabaee6552a2735b4de1b77ef8699e7
KamiyaNya <dikriss16@gmail.com>
2021-01-17 22:32:39 +0300

    Почистил от мусора

--- controllers/getArticles.js
+++ controllers/getArticles.js
@@ -3,16 +3,6 @@ const Article = require('../models/Article')
 module.exports.getArticles = async (req, res) => {
     try {
         const articles = await Article.find({}).lean()
-        // const articlesBody = await Article.findOne({
-        //     articleBody: articleBody
-        // })
-        // let shortArticleBody = ''
-
-        // for (let i = 0; i < articlesBody.length; i++) {
-        //     //Написать цикл по перебору срок из бд
-        //     shortArticleBody += articlesBody
-        // }
-        // console.log(shortArticleBody)
         res.render('articlesPage', {
             title: 'Новости',
             articles,
@@ -20,9 +10,6 @@ module.exports.getArticles = async (req, res) => {
         })
     } catch (e) {
         if (e) {
-            // res.status(503).json({
-            //     message: `Ошибка сервера : ${error}`
-            // })
             console.log(e)
         }
     }
@@ -35,8 +22,6 @@ module.exports.getArticleById = async (req, res, next) => {
         const articles = await Article.findById({
             _id: postId
         })
-        // res.json(req.params)
-        // console.log(postIdTrim)
         res.render('articlePage', {
             title: articles.articleTitle,
             id: postId,
