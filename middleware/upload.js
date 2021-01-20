6e0e5351e1e85b84d6af1478bc7439cf69780163
KamiyaNya <dikriss16@gmail.com>
2021-01-20 18:31:50 +0300

    Добавил middleware для обработки изображений

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,5 +1,27 @@
 const multer = require('multer')
 const moment = require('moment')
 
-
-module.exports = multer({})
+const storage = multer.diskStorage({
+    destination(req, file, cb) {
+        cb(null, "uploads/")
+    },
+    filename(req, file, cb) {
+        const date = moment().format('DDMMYYYY-HHmmss_SSS')
+        cb(null, `${date}-${file.originalname}`)
+    }
+})
+const fileFilters = (req, file, cb) => {
+    if (file.mimetype == 'image/png' || file.mimetype == 'image/jpeg') {
+        cb(null, true)
+    } else {
+        cb(null, false)
+    }
+}
+const limits = {
+    fileSize: 1024 * 1024 * 5
+}
+module.exports = multer({
+    storage: storage,
+    fileFilter: fileFilters,
+    limits: limits
+})
